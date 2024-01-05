410b00865f7c03f9112fe1d4e6482560f31533a3
lapaii <lapaii@lapaii.dev>
2024-01-05 02:44:08 +0000

    first website version

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      "hovercol": '#eb98a9',
+      "normalcol": '#e66378',
+    },
+  },
+  plugins: [],
+}
